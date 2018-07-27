7ac665522ab0f5afdbff262a3b91a1ce9ee0e8e8
v.spodin <v.spodin@pfsoft.org>
2018-07-27 10:21:26 +0300

    Добавьте файлы проекта.

---
+++ UITests/Test.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.IO;
+
+namespace UITests
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            string path = System.IO.Path.GetFullPath(@"..\..\..\");
+            // Initialize the Chrome DriverC:\Users\php\source\repos\UITests\UITests\Resources\
+            using (var driver = new ChromeDriver(path+@"\Resources"))
+            {
+                // Go to the home page
+                driver.Navigate().GoToUrl("http://testing-ground.scraping.pro/login");
+
+                // Get the page elements
+                var userNameField = driver.FindElementById("usr");
+                var userPasswordField = driver.FindElementById("pwd");
+                var loginButton = driver.FindElementByXPath("//input[@value='Login']");
+
+                // Type user name and password
+                userNameField.SendKeys("admin");
+                userPasswordField.SendKeys("12345");
+
+                // and click the login button
+                loginButton.Click();
+
+                // Extract the text and save it into result.txt
+                var result = driver.FindElementByXPath("//div[@id='case_login']/h3").Text;
+                File.WriteAllText("result.txt", result);
+
+                // Take a screenshot and save it into screen.png
+                driver.GetScreenshot().SaveAsFile(@"screen.png", ScreenshotImageFormat.Png);
+            }
+        }
+    }
+}
